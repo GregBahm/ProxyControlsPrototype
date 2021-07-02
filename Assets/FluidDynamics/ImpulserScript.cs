bd928077646f10a64d28428c77dfbef3d1e09607
Greg Bahm <grbahm@microsoft.com>
2021-07-01 21:52:14 -0700

    Adding fluids.

---
+++ Assets/FluidDynamics/ImpulserScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImpulserScript : MonoBehaviour 
+{
+    public float ForceMultiplier = 100;
+
+    [SerializeField]
+    private FluidSimulator simulator;
+
+	void Update ()
+    {
+        simulator.AddImpulse(transform.localPosition, transform.forward * ForceMultiplier);
+	}
+}
