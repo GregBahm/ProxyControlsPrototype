4c23d4f5a152ba7a01ccec935639c75bce5ab5a0
Greg Bahm <grbahm@microsoft.com>
2021-10-01 09:59:45 -0700

    Rotation is being difficult for some reason

--- Assets/SpectrographLineRenderer.cs
+++ Assets/SpectrographLineRenderer.cs
@@ -18,8 +18,11 @@ public class SpectrographLineRenderer : MonoBehaviour
     public Color StartColor;
     public Color EndColor;
 
-    [Range(0, 1)]
     public float Diskify;
+    public Vector3 RotationCenter;
+
+    [Range(0,1)]
+    public float Smoothing;
 
     private void Start()
     {
@@ -68,21 +71,53 @@ public class AudioLineBehavior : MonoBehaviour
     private void UpdatePosition()
     {
         lineRenderer.positionCount = Mothership.ResolutionPerLine;
+        Vector3[] basePositions = new Vector3[Mothership.ResolutionPerLine];
         for (int i = 0; i < Mothership.ResolutionPerLine; i++)
         {
-            Vector3 pos = GetPosition(i);
-            lineRenderer.SetPosition(i, pos);
+            basePositions[i] = GetBasePosition(i);
         }
+        Vector3[] finalPositions = new Vector3[Mothership.ResolutionPerLine];
+        for (int i = 0; i < Mothership.ResolutionPerLine; i++)
+        {
+            finalPositions[i] = GetFinalPosition(basePositions, i);
+        }
+        lineRenderer.SetPositions(finalPositions);
     }
 
-    private Vector3 GetPosition(int i)
+
+    //float angle = _Time.y;
+    //float newX = cos(angle) * dir.x - sin(angle) * dir.z;
+    //float newZ = sin(angle) * dir.x + cos(angle) * dir.z;
+    //return float3(newX, dir.y* 5, newZ);
+    private Vector3 GetFinalPosition(Vector3[] basePositions, int i)
+    {
+        float angle = (float)i / Mothership.ResolutionPerLine;
+        angle *= Mothership.Diskify;
+        Vector3 smoothedPosition = GetSmoothedPosition(basePositions, i);
+        smoothedPosition -= Mothership.RotationCenter;
+        float newX = Mathf.Cos(angle) * smoothedPosition.z - Mathf.Sin(angle) * smoothedPosition.x;
+        float newY = Mathf.Sin(angle) * smoothedPosition.z + Mathf.Cos(angle) * smoothedPosition.x;
+        Vector3 rotatedPoint = new Vector3(newY, smoothedPosition.y, newX);
+        rotatedPoint += Mothership.RotationCenter;
+        return rotatedPoint;
+    }
+
+    private Vector3 GetSmoothedPosition(Vector3[] basePositions, int i)
+    {
+        int previousIndex = Mathf.Max(0, i - 1);
+        int nextIndex = Mathf.Min(i + 1, Mothership.ResolutionPerLine - 2);
+        Vector3 prev = basePositions[previousIndex];
+        Vector3 next = basePositions[nextIndex];
+        Vector3 average = (prev + next) / 2;
+        Vector3 current = basePositions[i];
+        return Vector3.Lerp(current, average, Mothership.Smoothing);
+    }
+
+    private Vector3 GetBasePosition(int i)
     {
         float u = (float)i / Mothership.ResolutionPerLine;
         float v = LineParam;
-        Color col = Mothership.LineSource.GetPixelBilinear(u, v);
-
-
-
-        return new Vector3(v, col.r, u);
+        Color texSample = Mothership.LineSource.GetPixelBilinear(u, v);
+        return new Vector3(v, texSample.r, u);
     }
 }
