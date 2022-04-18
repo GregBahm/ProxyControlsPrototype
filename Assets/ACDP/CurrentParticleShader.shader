8f89bc96e8120700c7f2f35a64aeb0413fe3b928
Greg Bahm <grbahm@microsoft.com>
2022-04-18 14:55:21 -0700

    ACDP currents

---
+++ Assets/ACDP/CurrentParticleShader.shader
@@ -0,0 +1,104 @@
+﻿Shader "Unlit/CurrentParticleShader"
+{
+	Properties
+	{
+
+	}
+	SubShader
+	{
+		LOD 100
+
+		Pass
+		{
+			BlendOp Max
+			ZWrite Off
+			CGPROGRAM
+			#pragma vertex vert  
+			#pragma fragment frag
+			#pragma target 5.0
+			
+			#include "UnityCG.cginc"
+
+			struct ParticleComputeData
+			{
+				float2 currentPosition;
+				float2 previousPosition;
+				float time;
+			};
+
+			StructuredBuffer<float3> originalPositions;
+			StructuredBuffer<ParticleComputeData> particleComputeData;
+
+			float particleLifetime;
+			float particleSize;
+
+			struct v2f
+			{
+				float4 pos : SV_POSITION;
+				float3 particlePos: TEXCOORD0;
+				float velocity : TEXCOORD1;
+				float livetimeAlpha : TECOORD2;
+			};
+
+      float4x4 masterTransform;
+
+			float4x4 GetLookMatrix(float3 forward)
+			{
+				float3 xAxis = cross(forward, float3(0, 1, 0));
+				float3 yAxis = cross(forward, xAxis);
+
+				return float4x4( 
+					xAxis.x, yAxis.x, forward.x, 0,
+					xAxis.y, yAxis.y, forward.y, 0,
+					xAxis.z, yAxis.z, forward.z, 0,
+					0, 0, 0, 1
+					);
+			}
+
+			float3 GetRotatedVert(float3 vert, float2 currentPos, float2 previousPos)
+			{
+				float2 toNew = currentPos - previousPos;
+				float2 dir = normalize(toNew);
+				float3 forward = float3(dir.x, 0, dir.y);
+				float4x4 look = GetLookMatrix(forward);
+				float3 newVert = mul(look, vert);
+				return newVert;
+			}
+			
+			v2f vert (appdata_full v, uint inst : SV_InstanceID)
+			{
+				v2f o;
+
+				float3 originalPos = originalPositions[inst];
+				ParticleComputeData data = particleComputeData[inst];
+
+				float3 particlePos = float3(data.currentPosition.x, originalPos.y, data.currentPosition.y);
+
+				o.particlePos = particlePos;
+				float3 rotatedVert = GetRotatedVert(v.vertex.xyz, data.currentPosition, data.previousPosition);
+
+				float lifetime = data.time / particleLifetime;
+				float lifetimeAlpha = abs(lifetime - .5) * 2;
+				lifetimeAlpha = 1 - pow(lifetimeAlpha, 2);
+				o.livetimeAlpha = lifetimeAlpha;
+
+				rotatedVert *= lifetimeAlpha;
+				rotatedVert *= particleSize;
+				
+				float3 objPos = rotatedVert + float4(particlePos, 0);
+				o.pos = UnityObjectToClipPos(objPos);
+				o.velocity = length(data.currentPosition - data.previousPosition);
+				
+				return o;
+			}
+			
+			fixed4 frag(v2f i) : COLOR
+			{
+				float3 col = i.particlePos;
+				col *= i.livetimeAlpha;
+				return float4(col, 1);
+			} 
+			ENDCG
+		}
+	}
+}
---
+++ Assets/ACDP/CurrentSimulator.cs
@@ -0,0 +1,151 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class CurrentSimulator : MonoBehaviour
+{
+    public VolumeTextureGenerator TextureGenerator;
+
+    [SerializeField]
+    private Mesh arrowMesh;
+
+    [SerializeField]
+    private Vector3 baseCurrentForce;
+
+    [SerializeField]
+    private float ParticleSize;
+
+    [SerializeField]
+    private ComputeShader particleUpdater;
+
+    [SerializeField]
+    private Material particleMaterial;
+
+    [SerializeField]
+    private int particleCount;
+
+    [SerializeField]
+    private float particleLifetime = 1;
+
+    [SerializeField]
+    private float particleSpeed = .1f;
+
+
+    private Bounds bounds;
+
+    private ComputeBuffer originalPositions;
+    private ComputeBuffer particleComputeData;
+    private ComputeBuffer indirectArgs;
+
+    private const int GroupSize = 128;
+    private int updateParticlesKernel;
+
+    private const int originalPositionStride =  sizeof(float) * 3;// Original Position 
+
+    private const int particleStride =          sizeof(float) * 2 // Current Position 
+                                              + sizeof(float) * 2 // Previous Position
+                                              + sizeof(float);    // Time
+
+    struct ParticleComputeData
+    {
+        public Vector2 currentPosition;
+        public Vector2 previousPosition;
+        public float time;
+    };
+
+    void Start()
+    {
+        updateParticlesKernel = particleUpdater.FindKernel("UpdateParticles");
+
+        SetParticleBuffers();
+        indirectArgs = GetIndirectArgs();
+
+        Texture3D assembledTexture = TextureGenerator.GetAssembledTexture();
+        particleUpdater.SetTexture(updateParticlesKernel, "MapTexture", assembledTexture);
+        particleMaterial.SetTexture("MapTexture", assembledTexture);
+
+        bounds = GetComponent<BoxCollider>().bounds;
+    }
+
+    private Vector3 GetRandomPointInCube()
+    {
+        return new Vector3
+            (
+                UnityEngine.Random.value,
+                UnityEngine.Random.value,
+                UnityEngine.Random.value
+            );
+    }
+
+    private ComputeBuffer GetIndirectArgs()
+    {
+        ComputeBuffer ret = new ComputeBuffer(1, 5 * sizeof(uint), ComputeBufferType.IndirectArguments);
+        uint[] args = new uint[5] { 0, 0, 0, 0, 0 };
+        args[0] = arrowMesh.GetIndexCount(0);
+        args[1] = (uint)particleCount;
+        args[2] = arrowMesh.GetIndexStart(0);
+        args[3] = arrowMesh.GetBaseVertex(0);
+        ret.SetData(args);
+        return ret;
+    }
+    private void SetParticleBuffers()
+    {
+        originalPositions = new ComputeBuffer(particleCount, originalPositionStride);
+        particleComputeData = new ComputeBuffer(particleCount, particleStride);
+
+        Vector3[] originalPositionsData = new Vector3[particleCount];
+        for (int i = 0; i < particleCount; i++)
+        {
+            originalPositionsData[i] = GetRandomPointInCube();
+        }
+        originalPositions.SetData(originalPositionsData);
+
+        ParticleComputeData[] data = new ParticleComputeData[particleCount];
+        for (int i = 0; i < particleCount; i++)
+        {
+            Vector3 pos = originalPositionsData[i];
+            data[i].currentPosition = new Vector2(pos.x, pos.z);
+            data[i].previousPosition = new Vector2(pos.x, pos.z);
+            data[i].time = UnityEngine.Random.value * particleLifetime;
+        }
+        particleComputeData.SetData(data);
+    }
+
+    private void Update()
+    {
+        DispatchCompute();
+        DispatchRender();
+    }
+
+    private void DispatchRender()
+    {
+        particleMaterial.SetMatrix("masterTransform", transform.localToWorldMatrix);
+        particleMaterial.SetBuffer("originalPositions", originalPositions);
+        particleMaterial.SetBuffer("particleComputeData", particleComputeData);
+        particleMaterial.SetFloat("particleSize", ParticleSize);
+        particleMaterial.SetFloat("particleLifetime", particleLifetime);
+        particleMaterial.SetPass(0);
+        Graphics.DrawMeshInstancedIndirect(arrowMesh, 0, particleMaterial, bounds, indirectArgs);
+    }
+
+    private void DispatchCompute()
+    {
+        particleUpdater.SetBuffer(updateParticlesKernel, "originalPositions", originalPositions);
+        particleUpdater.SetBuffer(updateParticlesKernel, "particleComputeData", particleComputeData);
+
+        particleUpdater.SetFloat("deltaTime", Time.deltaTime);
+        particleUpdater.SetFloat("particleLifetime", particleLifetime);
+        particleUpdater.SetFloat("particleSpeed", particleSpeed);
+        particleUpdater.SetVector("baseCurrentForce", baseCurrentForce);
+
+        int numberofGroups = Mathf.CeilToInt((float)particleCount / GroupSize);
+        particleUpdater.Dispatch(updateParticlesKernel, numberofGroups, 1, 1);
+    }
+
+    void OnDestroy()
+	{
+        originalPositions.Dispose();
+        particleComputeData.Dispose();
+        indirectArgs.Dispose();
+	}
+}
